239f726287e55c5b627c1f311283651786b1ae65
NaweedSufranj <sufranj.naweed@gmail.com>
2019-08-08 12:13:31 +0200

    animation scroll bar & social

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
 console.log("init main");
+
+/// Progression Bar
+function progressionBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progression-bar").style.width = scrolled + "%";
+}
+
+/// Hide Social Media
+var prevScrollpos = window.pageYOffset;
+function hideSocialMedia(){
+    
+    var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("side-social").style.left = "10px";
+  } else {
+    document.getElementById("side-social").style.left = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+window.onscroll = function() {
+    progressionBar();
+    hideSocialMedia()
+}
