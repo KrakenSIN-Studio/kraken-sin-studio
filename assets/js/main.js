395074de8fc06a6537b7ad02132f285883193265
Kraken <sufranj.naweed@gmail.com>
2019-08-11 21:10:04 +0200

    try gitupage

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,22 +94,25 @@ document.addEventListener("DOMContentLoaded", function() {
   (function() {
     scrollTo();
   })();
-  
   function scrollTo() {
-    const links = document.querySelectorAll('.scroll');
-    links.forEach(each => (each.onclick = scrollAnchors));
+    var links = document.querySelectorAll('.scroll');
+    links.forEach(function(each){
+      each.onclick = scrollAnchors
+    });
   }
-  
-  function scrollAnchors(e, respond = null) {
-    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+  function scrollAnchors(e, respond) {
+    var distanceToTop = 
+      function(el){
+        return  Math.floor(el.getBoundingClientRect().top);
+      }
     e.preventDefault();
     var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-    const targetAnchor = document.querySelector(targetID);
+    var targetAnchor = document.querySelector(targetID);
     if (!targetAnchor) return;
-    const originalTop = distanceToTop(targetAnchor);
+    var originalTop = distanceToTop(targetAnchor);
     window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-    const checkIfDone = setInterval(function() {
-      const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    var checkIfDone = setInterval(function() {
+      var atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
       if (distanceToTop(targetAnchor) === 0 || atBottom) {
         targetAnchor.tabIndex = '-1';
         targetAnchor.focus();
